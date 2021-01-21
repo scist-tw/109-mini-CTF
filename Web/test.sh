95e4fc3e3c671e2863f469e3d7fb738c75d8ad1e
Allen Guan <tsguan11@gmail.com>
2021-01-21 10:41:54 +0800

    Update test.sh

--- Web/test.sh
+++ Web/test.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-echo
-echo Manager
-./Manager/solve.py
-echo
-echo PHP_was_best
-./PHP_was_best/solve.sh
-echo
-echo Serialize
-./Serialize/solve.py
-echo
-echo SSGo
-./SSGo/solve.py
-echo
+for dir in $(ls -d */); do
+  echo
+  echo $dir
+  ./$dir/solve*
+done
